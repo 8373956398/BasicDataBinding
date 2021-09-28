b5b761ea8bccd2fa9ffade63c1b2e9a9feaa5f65
Praveen <Praveen@DESKTOP-DD11LNH>
2021-09-28 15:50:07 +0530

    Add project files.

---
+++ BasicDataBinding/BasicDataBinding/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicDataBinding
+{
+    public class Data
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ BasicDataBinding/BasicDataBinding/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BasicDataBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<Data> people = new List<Data>()
+        {
+            new Data{Name="praveen", Address="Delhi"},
+            new Data{Name="abhinav", Address="nodia"},
+            new Data{Name="Ashwini", Address="greaternodia"},
+            new Data{Name="neelam", Address="Sagarpur"}
+        };
+        public MainPage()
+        {
+            InitializeComponent();
+            mylist.ItemsSource = people;
+
+        }
+    }
+}
